d19d9d031170675cba419256df1418ee20940252
ankit204 <arnold204@gmail.com>
2019-07-14 14:12:22 -0400

    adding implementation for level and spkears audio data api

--- src/audio/sc-analyzer.js
+++ src/audio/sc-analyzer.js
@@ -6,7 +6,8 @@ class ScAnalyzer {
         defOpts = {
             'fftSize' : 2048,
             'type' : 'frequency',
-            'arraySize' : 256
+            'arraySize' : 256,
+            'smoothing': 0.6
         };
         this.options = Object.assign(defOpts, options);
         this.setupNodes();
@@ -29,6 +30,21 @@ class ScAnalyzer {
                 this.getData = function() {
                     this.analyzer.getFloatFrequencyData(this.array);
                     return this.array;
+                };
+                break;
+            case 'level':
+                this.array = new Uint8Array(this.options.arraySize);
+                this.getData = function() {
+                    this.analyzer.getByteTimeDomainData(this.array);
+                    let data = this.array;
+                    let sqSum = 0;
+                    for (let i = 0; i < data.length; i++){
+                        sqSum += data[i] * data[i];
+                    }
+                    let rms = Math.sqrt(sqSum / data.length);
+                    rms = Math.max(rms, this.options.smoothing);
+                    let db = 20 * (Math.log(rms) / Math.LN10);
+                    return db;
                 };
                 break;
         }
