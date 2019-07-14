172604fa7810c9cbefe777dcaf18f1f4a20b9f31
ankit204 <arnold204@gmail.com>
2019-07-14 14:10:52 -0400

    removing ui functions

--- src/audio/sc-spectroscope.js
+++ src/audio/sc-spectroscope.js
@@ -22,43 +22,6 @@ class ScSpectroscope extends ScModule {
         return this.analyzerNode.getData();
     }
 
-    bindToCanvas(renderCtx) {
-        this.renderCtx = renderCtx;
-        this.renderer = setInterval(this.render.bind(this),
-                            this.options.renderRate);
-    }
-
-    unbindCanvas() {
-        let W = this.renderCtx.canvas.width;
-        let H = this.renderCtx.canvas.height;
-        this.renderCtx.clearRect(0,0,W,H);
-        clearInterval(this.renderer);
-    }
-
-    render() {
-        let renderCtx = this.renderCtx;
-        let data = this.getAudioData();
-        let length = data.length;
-        let fftSize = this.options.fftSize;
-        let W = renderCtx.canvas.width;
-        let H = renderCtx.canvas.height;
-        let minDb = this.analyzerNode.analyzer.minDecibels;
-        let maxDb = this.analyzerNode.analyzer.maxDecibels;
-        let scaleY = function (y) {
-            y = (y-minDb)/(maxDb-minDb);
-            return (1-y) * H;
-        }
-        renderCtx.clearRect(0,0,W,H);
-        renderCtx.beginPath();
-        renderCtx.fillStyle = 'rgba(0,0,0,.4)';
-        renderCtx.moveTo(0, H);
-        let range = fftSize
-        for (let i = 0; i <= fftSize; i++) {
-            renderCtx.lineTo(i, scaleY(data[i]));
-        }
-        renderCtx.lineTo(W, H);
-        renderCtx.fill();
-    }
 }
 
 export default ScSpectroscope;
