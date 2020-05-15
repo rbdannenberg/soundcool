89a1bb70885631af704c8e490d88e053ee6c1019
Amit Meena <amitmeena094@gmail.com>
2020-05-15 09:08:23 +0530

    database initialization changed

--- src/backend/db.js
+++ src/backend/db.js
@@ -18,47 +18,30 @@ if (process.env.MYSQL_HOST) {
   module.exports = connection;
 } else {
   const sqlite3 = require("sqlite3").verbose();
-  const fs = require("fs");
 
-  const path = "./soundcool.db";
-  let firstTime = false;
-  try {
-    if (!fs.existsSync(path)) {
-      firstTime = true;
-    }
-  } catch (err) {
-    console.error(err);
-  }
   let connection = new sqlite3.Database("soundcool.db", err => {
     if (err) {
       return console.error(err.message);
     }
   });
 
-  if (firstTime) {
-    connection.serialize(() => {
-      connection
-        .run(
-          "CREATE TABLE users(user_id integer PRIMARY KEY AUTOINCREMENT,name text NOT NULL, password text, email text)"
-        )
-        .run(
-          `INSERT INTO users
-              VALUES(100,'User 1','$2b$10$zoD.V4IYLhWmwz5Kld3gAu/jhYsPZYDYAWamQToQeC4tenQ1p1wwS','user1@welcome.com'),
-              (101,'User 2','$2b$10$9nG9wvklANoTJIlO.WBrzu0wh6oTHHjh8hviJQ0pj7eEcY3aN4mMC','user2@welcome.com')`
-        )
-        .run(
-          "CREATE TABLE audioSharing(user_id integer PRIMARY KEY,sharing BOOLEAN default 0)"
-        )
-        .run(
-          "CREATE TABLE presets(preset_id integer PRIMARY KEY AUTOINCREMENT, user integer,name text, location text, created_date DATETIME DEFAULT CURRENT_TIMESTAMP)"
-        )
-        .run(
-          "CREATE TABLE projects(project_id integer PRIMARY KEY AUTOINCREMENT, user integer, name text NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP ,description text NOT NULL, content text, sharedUsers text, isPublic BOOLEAN default 0 )"
-        )
-        .run(
-          "CREATE TABLE sounds(sound_id integer PRIMARY KEY AUTOINCREMENT, user integer, name text, fileLocation text, created_date DATETIME DEFAULT CURRENT_TIMESTAMP ,type text)"
-        );
-    });
-  }
+  connection.serialize(() => {
+    connection
+      .run(
+        "CREATE TABLE IF NOT EXISTS users(user_id integer PRIMARY KEY AUTOINCREMENT,name text NOT NULL, password text, email text)"
+      )
+      .run(
+        "CREATE TABLE IF NOT EXISTS audioSharing(user_id integer PRIMARY KEY,sharing BOOLEAN default 0)"
+      )
+      .run(
+        "CREATE TABLE IF NOT EXISTS presets(preset_id integer PRIMARY KEY AUTOINCREMENT, user integer,name text, location text, created_date DATETIME DEFAULT CURRENT_TIMESTAMP)"
+      )
+      .run(
+        "CREATE TABLE IF NOT EXISTS projects(project_id integer PRIMARY KEY AUTOINCREMENT, user integer, name text NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP ,description text NOT NULL, content text, sharedUsers text, isPublic BOOLEAN default 0 )"
+      )
+      .run(
+        "CREATE TABLE IF NOT EXISTS sounds(sound_id integer PRIMARY KEY AUTOINCREMENT, user integer, name text, fileLocation text, created_date DATETIME DEFAULT CURRENT_TIMESTAMP ,type text)"
+      );
+  });
   module.exports = connection;
 }
