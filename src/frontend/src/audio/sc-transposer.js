9158c4f7ea13ed6bb3cf2b244ebbde6bf87a06c9
ankit204 <arnold204@gmail.com>
2020-06-21 19:17:00 -0400

    adding ScTransposer

---
+++ src/frontend/src/audio/sc-transposer.js
@@ -0,0 +1,21 @@
+import ScModule from "./sc-module.js";
+import ScPitch from "./sc-pitch-phasor.js";
+
+class ScTransposer extends ScPitch {
+  constructor(context, options={}) {
+    super(context);
+    let defOpts = {
+      pitchShift: 0
+    };
+    this.options = Object.assign(defOpts, options);
+    this.pitchShift = this.options.pitchShift;
+  }
+
+  set pitchShift(cents) {
+    cents = parseInt(cents);
+    this.pitch = cents;
+    this.options.pitchShift = cents;
+  }
+}
+
+export default ScTransposer;
